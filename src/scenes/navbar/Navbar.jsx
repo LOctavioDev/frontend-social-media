f01448897596efd1150c9c2d4cc32950a87c42b2
Luis Octavio <141202473+LOctavioDev@users.noreply.github.com>
2024-05-30 18:06:15 -0600

    MAIN

--- src/scenes/navbar/Navbar.jsx
+++ src/scenes/navbar/Navbar.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useSelector, useDispatch } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../../components/FlexBetween";
+
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isMobileToggledMenu, setIsMobileToggledMenu] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const theme = useTheme();
+
+  return <div>Navbar</div>;
+};
